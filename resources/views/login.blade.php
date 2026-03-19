a9c7ce9601c58a7da0e4996007fa9512ceb67743
Ranajit Rakshit <msdranajit@gmail.com>
2026-03-19 13:19:41 +0530

    comment removed

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Login - Grievance Portal</title>
-    <!-- Tailwind CSS Play CDN (Great for rapid prototyping) -->
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
