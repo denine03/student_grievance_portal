2b08b3a6622fb785a79897278fad014216411b8b
Timothy VL Ruatdika <159428594+Timothy5912@users.noreply.github.com>
2026-03-22 17:57:09 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('homepage');
 });
 
+Route::get('/register', function () {
+    return view('register');
+});
+
 Route::get('/login', function () {
     return view('login');
 });
